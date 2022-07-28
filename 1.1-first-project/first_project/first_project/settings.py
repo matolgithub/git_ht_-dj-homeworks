122ff17b92f3654f0a463619c11e22e68884c2ce
matolgithub <89175955650@mail.ru>
2022-07-28 19:23:21 +0300

    Move string of code with time_zone.

--- 1.1-first-project/first_project/first_project/settings.py
+++ 1.1-first-project/first_project/first_project/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -122,4 +122,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TIME_ZONE = 'Europe/Moscow'
