ed03327892b68fbba5595857460f552b57a83bad
matolgithub <89175955650@mail.ru>
2022-08-14 15:17:08 +0300

    Commit: change models.

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -14,3 +14,21 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    name = models.CharField(max_length=80)
+    articles = models.ManyToManyField(Article, related_name='scopes', through='ScopeArticle')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+
+class ScopeArticle(models.Model):
+    article = models.ForeignKey(Article, related_name='scope_article', on_delete=models.CASCADE)
+    scope = models.ForeignKey(Scope, related_name='scope_article', on_delete=models.CASCADE)
+    is_main = models.BooleanField()
