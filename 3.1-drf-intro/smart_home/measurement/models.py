71e50a6a0bc6fbed3901794a03fb526401e27984
matolgithub <89175955650@mail.ru>
2022-08-23 16:03:51 +0300

    Commit: models.py, serializers.py change code.

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+
+class Measurement(models.Model):
+    temperature = models.FloatField(max_length=100.00, null=False)
+    data_create = models.DateTimeField(auto_now_add=True)
+    data_update = models.DateTimeField(auto_now=True)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -1,3 +1,15 @@
 from rest_framework import serializers
 
-# TODO: опишите необходимые сериализаторы
+from measurement.models import Sensor, Measurement
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description']
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['temperature', 'data_create', 'data_update', 'sensor']
