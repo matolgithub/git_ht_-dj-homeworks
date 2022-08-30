5bd144cf91db3694958cfb4010b4ad680753708f
matolgithub <89175955650@mail.ru>
2022-08-30 17:24:05 +0300

    Commit: made views.py.

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,12 +1,26 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import AnonRateThrottle
 from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
 
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
 
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+
+    serializer_class = AdvertisementSerializer
+
+    filterset_class = AdvertisementFilter
+
+    filter_backends = [DjangoFilterBackend]
+
+    throttle_classes = [AnonRateThrottle]
+
 
     def get_permissions(self):
         """Получение прав для действий."""
