c10dab290b5b5b7b814ef8ff920070d17eec59fb
matolgithub <89175955650@mail.ru>
2022-09-03 17:43:45 +0300

    Commit: change settings.py, and made preform for tests.

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -128,3 +128,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MAX_STUDENTS_PER_COURSE = 20
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -5,9 +5,11 @@ from model_bakery import baker
 from students.models import Student, Course
 
 
-def test_example():
-    assert True, "Just test example"
+# def test_example():
+#     assert True, "Just test example"
 
+
+# ---------Fixtures and factories block
 
 @pytest.fixture
 def client():
@@ -28,3 +30,93 @@ def course_factory():
         return baker.make(Course, *args, **kwargs)
 
     return factory
+
+
+# -----------Tests block
+
+# Test_1
+@pytest.mark.django_db
+def test_1():
+    # Arrange
+    pass
+    # Act
+    pass
+    # Assert
+    pass
+
+
+# Test_2
+@pytest.mark.django_db
+def test_2():
+    # Arrange
+    pass
+    # Act
+    pass
+    # Assert
+    pass
+
+
+# Test_3
+@pytest.mark.django_db
+def test_3():
+    # Arrange
+    pass
+    # Act
+    pass
+    # Assert
+    pass
+
+
+# Test_4
+@pytest.mark.django_db
+def test_4():
+    # Arrange
+    pass
+    # Act
+    pass
+    # Assert
+    pass
+
+
+# Test_5
+@pytest.mark.django_db
+def test_5():
+    # Arrange
+    pass
+    # Act
+    pass
+    # Assert
+    pass
+
+
+# Test_6
+@pytest.mark.django_db
+def test_6():
+    # Arrange
+    pass
+    # Act
+    pass
+    # Assert
+    pass
+
+
+# Test_7
+@pytest.mark.django_db
+def test_7():
+    # Arrange
+    pass
+    # Act
+    pass
+    # Assert
+    pass
+
+
+# Test_8
+@pytest.mark.django_db
+def test_8():
+    # Arrange
+    pass
+    # Act
+    pass
+    # Assert
+    pass
