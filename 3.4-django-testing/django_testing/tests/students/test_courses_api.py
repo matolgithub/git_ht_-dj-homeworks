dc89394e7be7cdaf637f61acb9ebec320b7716b6
matolgithub <89175955650@mail.ru>
2022-09-03 16:27:53 +0300

    Commit: made fixture, factories and .coveragerc.

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,30 @@
+import pytest
+from rest_framework.test import APIClient
+from model_bakery import baker
+
+from students.models import Student, Course
+
+
 def test_example():
-    assert False, "Just test example"
+    assert True, "Just test example"
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture()
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+
+    return factory
+
+
+@pytest.fixture()
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+
+    return factory
